49ddeba1bfac2b423a0399b20141f84476dd74c2
Andrew Chou <andrewchou@fastmail.com>
2024-11-26 11:30:52 +0000

    prebuild

--- android/app/src/main/java/com/andrewchou/nodejsmobileexpoexample/MainActivity.kt
+++ android/app/src/main/java/com/andrewchou/nodejsmobileexpoexample/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.nodejsmobileexpoexample
+package com.andrewchou.nodejsmobileexpoexample
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/andrewchou/nodejsmobileexpoexample/MainApplication.kt
+++ android/app/src/main/java/com/andrewchou/nodejsmobileexpoexample/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.nodejsmobileexpoexample
+package com.andrewchou.nodejsmobileexpoexample
 
 import android.app.Application
 import android.content.res.Configuration
