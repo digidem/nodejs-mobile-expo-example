be84967981b3059d056a75017654612716c680a5
Andrew Chou <andrewchou@fastmail.com>
2024-11-26 12:32:43 +0000

    prebuild stuff

--- nodejs-assets/nodejs-project/main.js
+++ nodejs-assets/nodejs-project/main.js
@@ -1,12 +1,15 @@
 // Rename this sample file to main.js to use on your project.
 // The main.js file will be overwritten in updates/reinstalls.
 
-var rn_bridge = require('rn-bridge');
+var rn_bridge = require("rn-bridge");
+var sodium = require("sodium-native");
+
+console.log(sodium);
 
 // Echo every message received from react-native.
-rn_bridge.channel.on('message', (msg) => {
+rn_bridge.channel.on("message", (msg) => {
   rn_bridge.channel.send(msg);
-} );
+});
 
 // Inform react-native node is initialized.
 rn_bridge.channel.send("Node was initialized.");
